3c1c30ee80446b2c19984b4b2de1389a90ba07a5
arcuri82 <arcuri82@gmail.com>
2017-07-03 14:07:50 +0200

    embedded EM for features-service

---
+++ em/embedded/rest/features-service/src/main/java/org/javiermf/features/evomaster/EmbeddedEvoMasterController.java
@@ -0,0 +1,126 @@
+package org.javiermf.features.evomaster;
+
+
+import com.p6spy.engine.spy.P6SpyDriver;
+import org.evomaster.clientJava.controller.EmbeddedSutController;
+import org.evomaster.clientJava.controller.InstrumentedSutStarter;
+import org.evomaster.clientJava.controller.db.DbCleaner;
+import org.evomaster.clientJava.controllerApi.dto.AuthenticationDto;
+import org.hibernate.dialect.H2Dialect;
+import org.javiermf.features.Application;
+import org.springframework.boot.SpringApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Class used to start/stop the SUT. This will be controller by the EvoMaster process
+ */
+public class EmbeddedEvoMasterController extends EmbeddedSutController {
+
+    public static void main(String[] args) {
+
+        int port = 40100;
+        if (args.length > 0) {
+            port = Integer.parseInt(args[0]);
+        }
+
+        EmbeddedEvoMasterController controller = new EmbeddedEvoMasterController(port);
+        InstrumentedSutStarter starter = new InstrumentedSutStarter(controller);
+
+        starter.start();
+    }
+
+
+    private ConfigurableApplicationContext ctx;
+    private Connection connection;
+
+
+    public EmbeddedEvoMasterController() {
+        this(40100);
+    }
+
+    public EmbeddedEvoMasterController(int port) {
+        setControllerPort(port);
+    }
+
+    @Override
+    public String startSut() {
+
+        ctx = SpringApplication.run(Application.class, new String[]{
+                "--server.port=0",
+                "--spring.datasource.url=jdbc:p6spy:h2:mem:testdb;DB_CLOSE_DELAY=-1;",
+                "--spring.datasource.driver-class-name=" + P6SpyDriver.class.getName(),
+                "--spring.jpa.database-platform=" + H2Dialect.class.getName(),
+                "--spring.datasource.username=sa",
+                "--spring.datasource.password"
+        });
+
+        if (connection != null) {
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        JdbcTemplate jdbc = ctx.getBean(JdbcTemplate.class);
+
+        try {
+            connection = jdbc.getDataSource().getConnection();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        return "http://localhost:" + getSutPort();
+    }
+
+    protected int getSutPort() {
+        return (Integer) ((Map) ctx.getEnvironment()
+                .getPropertySources().get("server.ports").getSource())
+                .get("local.server.port");
+    }
+
+
+    @Override
+    public boolean isSutRunning() {
+        return ctx != null && ctx.isRunning();
+    }
+
+    @Override
+    public void stopSut() {
+        ctx.stop();
+    }
+
+    @Override
+    public String getPackagePrefixesToCover() {
+        return "org.javiermf.features.";
+    }
+
+    @Override
+    public void resetStateOfSUT() {
+        DbCleaner.clearDatabase_H2(connection);
+    }
+
+    @Override
+    public String getUrlOfSwaggerJSON() {
+        return "http://localhost:" + getSutPort() + "/swagger.json";
+    }
+
+    @Override
+    public List<AuthenticationDto> getInfoForAuthentication() {
+        return null;
+    }
+
+    public Connection getConnection() {
+        return connection;
+    }
+
+    @Override
+    public String getDatabaseDriverName() {
+        return "org.h2.Driver";
+    }
+}
