878e1def12d8efb1ef69cb5699693d66d81aa2bb
arcuri82 <arcuri82@gmail.com>
2019-04-03 13:49:44 +0200

    removed unnecessary initialization of DB table

--- em/external/rest/scout-api/src/main/resources/init_db.sql
+++ em/external/rest/scout-api/src/main/resources/init_db.sql
@@ -1,11 +1,3 @@
-
-insert into tag (id, grp, media_file_id, name) values (null, 'feelings', null, 'Happy');
-insert into tag (id, grp, media_file_id, name) values (null, 'feelings', null, 'Sad');
-insert into tag (id, grp, media_file_id, name) values (null, 'feelings', null, 'Bored');
-insert into tag (id, grp, media_file_id, name) values (null, 'feelings', null, 'Think');
-insert into tag (id, grp, media_file_id, name) values (null, 'feelings', null, 'Excited');
-
-
 insert into USERS (id, authorization_level, date_created, email_address, name) values (1, 0, '2001-02-03', null, 'INTEGRATION TEST USER');
 insert into USERS (id, authorization_level, date_created, email_address, name) values (2, 10, '2001-02-03', null, 'INTEGRATION TEST MODERATOR');
 insert into USERS (id, authorization_level, date_created, email_address, name) values (3, 20, '2001-02-03', null, 'INTEGRATION TEST ADMINISTRATOR');
