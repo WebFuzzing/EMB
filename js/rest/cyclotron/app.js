ff0ab7413955024877aafb29f44e5b10ba1f96ce
arcuri82 <arcuri82@gmail.com>
2020-02-22 18:26:09 +0100

    cyclotron: splitting app/server, and adding EM

--- js/rest/cyclotron/app.js
+++ js/rest/cyclotron/app.js
@@ -120,8 +120,4 @@ if ('development' == app.get('env')) {
 var api = require('./routes/api');
 api.bindRoutes(app);
 
-/* Start server */
-var port = app.get('port');
-app.listen(port, function(){
-    console.log('Cyclotron running on port %d', port);
-});
+module.exports = app;
