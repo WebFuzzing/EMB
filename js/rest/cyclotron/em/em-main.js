65ec3eeca88bf4c5eac658b8cb84ee392f1d7e61
arcuri82 <arcuri82@gmail.com>
2020-02-23 23:50:13 +0100

    choose port for JS controllers

--- js/rest/cyclotron/em/em-main.js
+++ js/rest/cyclotron/em/em-main.js
@@ -4,6 +4,8 @@ const em = require("evomaster-client-js");
 
 const controller = new em.EMController(new AppController());
 
+port = process.env.EM_PORT || 40100;
+controller.setPort(port);
 
 controller.startTheControllerServer();
 
--- js/rest/ncs/em/em-main.js
+++ js/rest/ncs/em/em-main.js
@@ -5,6 +5,9 @@ const em = require("evomaster-client-js");
 const controller = new em.EMController(new AppController());
 
 
+port = process.env.EM_PORT || 40100;
+controller.setPort(port);
+
 controller.startTheControllerServer();
 
 
