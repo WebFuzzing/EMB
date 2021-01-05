bb2dbb5f3233121716c2af357b336f38ae4b080b
Man Zhang <m.zhang1126@gmail.com>
2021-01-05 22:44:30 +0100

    add em that can be stopped with timer

--- js/rest/cyclotron/em/em-main.js
+++ js/rest/cyclotron/em/em-main.js
@@ -5,13 +5,3 @@ const controller = new em.EMController(new AppController());
 port = process.env.EM_PORT || 40100;
 controller.setPort(port);
 controller.startTheControllerServer();
-
-
-
-function signalHandle(signal) {
-    console.log('Received signal ', signal);
-    controller.stopTheControllerServer();
-}
-
-process.on('SIGINT', signalHandle);
-process.on('SIGTERM', signalHandle);
