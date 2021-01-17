a56c8890b5839610be4bb7a790a705a25fdcdfa5
amid <golmohammadi.edu@gmail.com>
2021-01-17 23:04:20 +0330

    Add drvier for SampleProject

--- dotnet/cs/rest/SampleProject/src/SampleProject.API/Program.cs
+++ dotnet/cs/rest/SampleProject/src/SampleProject.API/Program.cs
@@ -1,17 +1,46 @@
-﻿using Microsoft.AspNetCore;
+﻿using System;
+using System.Collections.Concurrent;
+using System.Threading;
+using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 
-namespace SampleProject.API
-{
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateWebHostBuilder(args).Build().Run();
+namespace SampleProject.API {
+    public class Program {
+        private static ConcurrentDictionary<int, CancellationTokenSource> tokens = new ConcurrentDictionary<int, CancellationTokenSource> ();
+
+        public static void Main (string[] args) {
+
+            if (args.Length > 0) {
+
+                int port = Convert.ToInt32 (args[0]);
+
+                tokens.TryAdd (port, new CancellationTokenSource ());
+
+                var host = CreateWebHostBuilder (args).Build ();
+
+                host.RunAsync (tokens[port].Token).GetAwaiter ().GetResult ();
+            } else {
+                var host = CreateWebHostBuilder (args).Build ();
+
+                host.RunAsync ().GetAwaiter ().GetResult ();
+            }
         }
 
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+        public static IWebHostBuilder CreateWebHostBuilder (string[] args) {
+
+            var webHostBuilder = WebHost.CreateDefaultBuilder (args)
+                .UseStartup<Startup> ();
+
+            return args.Length > 0 ? webHostBuilder.UseUrls ($"http://*:{args[0]}") : webHostBuilder;
+        }
+
+        public static void Shutdown () {
+
+            foreach (var pair in tokens) {
+                pair.Value.Cancel ();
+            }
+
+            tokens.Clear ();
+        }
     }
 }
---
+++ dotnet/em/embedded/rest/SampleProject/EmbeddedEvoMasterController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Controller;
+using Controller.Api;
+using Controller.Problem;
+
+namespace SampleProject
+{
+    public class EmbeddedEvoMasterController : EmbeddedSutController {
+
+        private bool isSutRunning;
+        private int sutPort;
+
+        static void Main (string[] args) {
+
+            var embeddedEvoMasterController = new EmbeddedEvoMasterController ();
+
+            InstrumentedSutStarter instrumentedSutStarter = new InstrumentedSutStarter (embeddedEvoMasterController);
+
+            System.Console.WriteLine ("Driver is starting...\n");
+
+            instrumentedSutStarter.Start ();
+        }
+
+        public override string GetDatabaseDriverName () => null;
+
+        public override List<AuthenticationDto> GetInfoForAuthentication () => null;
+
+        public override string GetPackagePrefixesToCover () => "SampleProject.API";
+
+        //TODO: later on we should create sth specific for C#
+        public override OutputFormat GetPreferredOutputFormat () => OutputFormat.JAVA_JUNIT_5;
+
+        //TODO: check again
+        public override IProblemInfo GetProblemInfo () =>
+            GetSutPort () != 0 ? new RestProblem ("http://localhost:" + GetSutPort () + "/swagger/v1/swagger.json", null) : new RestProblem (null, null);
+
+        public override bool IsSutRunning () => isSutRunning;
+
+        public override void ResetStateOfSut () { }
+
+        //This method in java client is not async
+        public override async Task<string> StartSutAsync () {
+            //TODO: check this again
+            int ephemeralPort = GetEphemeralTcpPort ();
+
+            var task = Task.Run (() => {
+
+                SampleProject.API.Program.Main (new string[] { ephemeralPort.ToString () });
+            });
+
+            await WaitUntilSutIsRunningAsync (ephemeralPort);
+
+            sutPort = ephemeralPort;
+
+            isSutRunning = true;
+
+            return $"http://localhost:{ephemeralPort}";
+        }
+
+        public override void StopSut () {
+
+            SampleProject.API.Program.Shutdown ();
+
+            isSutRunning = false;
+        }
+
+        protected int GetSutPort () => sutPort;
+    }
+}
