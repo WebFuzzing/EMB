958b51daa877a22fc46668a85563db444da47ced
arcuri82 <arcuri82@gmail.com>
2021-03-23 13:32:22 +0100

    driver for Petclinic

--- jdk_8_maven/em/embedded/graphql/spring-petclinic-graphql/src/main/java/em/embedded/org/springframework/samples/petclinic/EmbeddedEvoMasterController.java
+++ jdk_8_maven/em/embedded/graphql/spring-petclinic-graphql/src/main/java/em/embedded/org/springframework/samples/petclinic/EmbeddedEvoMasterController.java
@@ -1,64 +1,147 @@
 package em.embedded.org.springframework.samples.petclinic;
 
+import com.p6spy.engine.spy.P6SpyDriver;
 import org.evomaster.client.java.controller.EmbeddedSutController;
+import org.evomaster.client.java.controller.InstrumentedSutStarter;
 import org.evomaster.client.java.controller.api.dto.AuthenticationDto;
 import org.evomaster.client.java.controller.api.dto.SutInfoDto;
+import org.evomaster.client.java.controller.db.DbCleaner;
+import org.evomaster.client.java.controller.db.SqlScriptRunnerCached;
+import org.evomaster.client.java.controller.internal.SutController;
+import org.evomaster.client.java.controller.problem.GraphQlProblem;
 import org.evomaster.client.java.controller.problem.ProblemInfo;
+import org.springframework.boot.SpringApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.samples.petclinic.PetClinicApplication;
+import org.testcontainers.containers.GenericContainer;
 
 import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 
 public class EmbeddedEvoMasterController extends EmbeddedSutController {
 
+    public static void main(String[] args){
+
+        int port = 40100;
+        if (args.length > 0) {
+            port = Integer.parseInt(args[0]);
+        }
+
+        SutController controller = new EmbeddedEvoMasterController(port);
+        InstrumentedSutStarter starter = new InstrumentedSutStarter(controller);
+
+        starter.start();
+    }
+
+    private ConfigurableApplicationContext ctx;
+    private Connection connection;
+
+    private static final GenericContainer postgres = new GenericContainer("postgres:9")
+            .withExposedPorts(5432)
+            .withEnv("POSTGRES_HOST_AUTH_METHOD","trust")
+            .withTmpFs(Collections.singletonMap("/var/lib/postgresql/data", "rw"));
+
+
+    public EmbeddedEvoMasterController() {
+        this(40100);
+    }
+
+    public EmbeddedEvoMasterController(int port) {
+        setControllerPort(port);
+    }
+
+
+    @Override
+    public String startSut() {
+
+        String host = postgres.getContainerIpAddress();
+        int port = postgres.getMappedPort(5432);
+        String url = "jdbc:p6spy:postgresql://"+host+":"+port+"/postgres";
+
+        ctx = SpringApplication.run(PetClinicApplication.class, new String[]{
+                "--server.port=0",
+                "--spring.datasource.url=" + url,
+                "--spring.datasource.driver-class-name=" + P6SpyDriver.class.getName(),
+                "--spring.datasource.username=sa",
+                "--spring.datasource.password",
+                "--spring.cache.type=none",
+                "--spring.profiles.active=postgresql,spring-data-jpa",
+                "--spring.datasource.initialize=true",
+                "--spring.datasource.schema=classpath*:db/postgresql/initDB.sql"
+        });
+
+        if (connection != null) {
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        JdbcTemplate jdbc = ctx.getBean(JdbcTemplate.class);
+
+        try {
+            connection = jdbc.getDataSource().getConnection();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        return "http://localhost:" + getSutPort();
+    }
+
+    protected int getSutPort() {
+        return (Integer) ((Map) ctx.getEnvironment()
+                .getPropertySources().get("server.ports").getSource())
+                .get("local.server.port");
+    }
 
 
     @Override
     public boolean isSutRunning() {
-        return false;
+        return ctx != null && ctx.isRunning();
+    }
+
+    @Override
+    public void stopSut() {
+        ctx.stop();
     }
 
     @Override
     public String getPackagePrefixesToCover() {
-        return null;
+        return "org.springframework.samples.petclinic";
     }
+
+    @Override
+    public void resetStateOfSUT() {
+        DbCleaner.clearDatabase_Postgres(connection);
+        SqlScriptRunnerCached.runScriptFromResourceFile(connection,"/db/postgresql/populateDB.sql");
+    }
+
+    @Override
+    public ProblemInfo getProblemInfo() {
+        return new GraphQlProblem("/graphql");
+    }
+
+    @Override
+    public SutInfoDto.OutputFormat getPreferredOutputFormat() {
+        return SutInfoDto.OutputFormat.JAVA_JUNIT_4;
+    }
+
 
     @Override
     public List<AuthenticationDto> getInfoForAuthentication() {
         return null;
     }
 
-    @Override
     public Connection getConnection() {
-        return null;
+        return connection;
     }
 
     @Override
     public String getDatabaseDriverName() {
-        return null;
-    }
-
-    @Override
-    public ProblemInfo getProblemInfo() {
-        return null;
-    }
-
-    @Override
-    public SutInfoDto.OutputFormat getPreferredOutputFormat() {
-        return null;
-    }
-
-    @Override
-    public String startSut() {
-        return null;
-    }
-
-    @Override
-    public void stopSut() {
-
-    }
-
-    @Override
-    public void resetStateOfSUT() {
-
+        return "org.postgresql.Driver";
     }
 }
