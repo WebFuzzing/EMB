f1af260991068b167ed1169a8f33de4c44909cf5
Amid Gol <golmohammadi.edu@gmail.com>
2021-04-12 01:54:33 +0430

    Enable starting SCS SUT from the driver

---
+++ dotnet_3/em/embedded/rest/ScsDriver/EmbeddedEvoMasterController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using EvoMaster.Controller;
+using EvoMaster.Controller.Api;
+using EvoMaster.Controller.Problem;
+
+namespace ScsDriver
+{
+    public class EmbeddedEvoMasterController : EmbeddedSutController
+    {
+        private bool _isSutRunning;
+        private int _sutPort;
+
+        static void Main(string[] args)
+        {
+            var embeddedEvoMasterController = new EmbeddedEvoMasterController();
+
+            var instrumentedSutStarter = new InstrumentedSutStarter(embeddedEvoMasterController);
+
+            Console.WriteLine("Driver is starting...\n");
+
+            instrumentedSutStarter.Start();
+        }
+
+        public override string StartSut()
+        {
+            var ephemeralPort = GetEphemeralTcpPort();
+
+            Task.Run(() =>
+            {
+                SCS.Program.Main(new[] {ephemeralPort.ToString()});
+            });
+
+            WaitUntilSutIsRunning(ephemeralPort);
+
+            _sutPort = ephemeralPort;
+
+            _isSutRunning = true;
+
+            return $"http://localhost:{ephemeralPort}";
+        }
+
+        public override void StopSut()
+        {
+            SCS.Program.Shutdown();
+            _isSutRunning = false;
+        }
+
+        public override bool IsSutRunning() => _isSutRunning;
+
+        public override string GetPackagePrefixesToCover() => "SCS";
+
+        public override List<AuthenticationDto> GetInfoForAuthentication() => null;
+
+        public override string GetDatabaseDriverName() => null;
+
+        //TODO: check
+        public override IProblemInfo GetProblemInfo() =>
+            GetSutPort() != 0
+                ? new RestProblem("http://localhost:" + GetSutPort() + "/swagger/v1/swagger.json", null)
+                : new RestProblem(null, null);
+
+        public override OutputFormat GetPreferredOutputFormat() => OutputFormat.CSHARP_XUNIT;
+
+        public override void ResetStateOfSut()
+        {
+        }
+
+        protected int GetSutPort() => _sutPort;
+    }
+}
