af1b2edf35e31634f9615c9b8ce65330e59df8bf
ZhangMan <m.zhang1126@gmail.com>
2021-10-24 00:33:08 +0200

    add test

---
+++ jdk_8_maven/cs/rpc/thrift/artificial/thrift-ncs/src/test/java/org/thrift/ncs/NcsServiceTest.java
@@ -0,0 +1,29 @@
+package org.thrift.ncs;
+
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * created by manzhang on 2021/10/21
+ */
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(classes = NcsApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class NcsServiceTest {
+
+    @Autowired
+    protected NcsServiceImpl service;
+
+
+    @Test
+    public void testTriangleByNativeService() throws Exception {
+        Dto dto = service.checkTriangle(3, 4,5);
+        assertEquals(1, dto.resultAsInt);
+    }
+
+}
---
+++ jdk_8_maven/cs/rpc/thrift/artificial/thrift-scs/src/test/java/org/thrift/scs/ScsServiceTest.java
@@ -0,0 +1,28 @@
+package org.thrift.scs;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * created by manzhang on 2021/10/23
+ */
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(classes = ScsApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class ScsServiceTest {
+
+    @Autowired
+    protected ScsServiceImpl service;
+
+
+    @Test
+    public void testCalcByNativeService() throws Exception {
+        String value = service.calc("plus", 1, 2);
+        assertEquals("3.0", value);
+    }
+
+}
