5b1ad5f81b1147cdd822572581105aa27df32c12
arcuri82 <arcuri82@gmail.com>
2021-10-31 20:42:03 +0100

    preparing embedded driver

---
+++ jdk_11_mvn/em/embedded/rest/cwa-verification/src/main/java/em/embedded/app/coronawarn/verification/EmbeddedEvoMasterController.java
@@ -0,0 +1,129 @@
+package em.embedded.app.coronawarn.verification;
+
+
+import app.coronawarn.verification.VerificationApplication;
+import org.evomaster.client.java.controller.EmbeddedSutController;
+import org.evomaster.client.java.controller.InstrumentedSutStarter;
+import org.evomaster.client.java.controller.api.dto.AuthenticationDto;
+import org.evomaster.client.java.controller.api.dto.SutInfoDto;
+import org.evomaster.client.java.controller.db.DbCleaner;
+import org.evomaster.client.java.controller.problem.ProblemInfo;
+import org.evomaster.client.java.controller.problem.RestProblem;
+import org.springframework.boot.SpringApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Class used to start/stop the SUT. This will be controller by the EvoMaster process
+ */
+public class EmbeddedEvoMasterController extends EmbeddedSutController {
+
+    public static void main(String[] args) {
+
+        int port = 40100;
+        if (args.length > 0) {
+            port = Integer.parseInt(args[0]);
+        }
+
+        EmbeddedEvoMasterController controller = new EmbeddedEvoMasterController(port);
+        InstrumentedSutStarter starter = new InstrumentedSutStarter(controller);
+
+        starter.start();
+    }
+
+
+    private ConfigurableApplicationContext ctx;
+    private Connection connection;
+
+
+    public EmbeddedEvoMasterController() {
+        this(40100);
+    }
+
+    public EmbeddedEvoMasterController(int port) {
+        setControllerPort(port);
+    }
+
+    @Override
+    public String startSut() {
+
+        ctx = SpringApplication.run(VerificationApplication.class, new String[]{
+                "--server.port=0",
+                "--spring.profiles.active=local"
+        });
+
+        if (connection != null) {
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        JdbcTemplate jdbc = ctx.getBean(JdbcTemplate.class);
+
+        try {
+            connection = jdbc.getDataSource().getConnection();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        return "http://localhost:" + getSutPort();
+    }
+
+    protected int getSutPort() {
+        return (Integer) ((Map) ctx.getEnvironment()
+                .getPropertySources().get("server.ports").getSource())
+                .get("local.server.port");
+    }
+
+
+    @Override
+    public boolean isSutRunning() {
+        return ctx != null && ctx.isRunning();
+    }
+
+    @Override
+    public void stopSut() {
+        ctx.stop();
+    }
+
+    @Override
+    public String getPackagePrefixesToCover() {
+        return "app.coronawarn.verification.";
+    }
+
+    @Override
+    public void resetStateOfSUT() {
+        DbCleaner.clearDatabase_H2(connection);
+    }
+
+    @Override
+    public ProblemInfo getProblemInfo() {
+        return new RestProblem(
+                "http://localhost:" + getSutPort() + "/api/docs",
+                null
+        );
+    }
+
+    @Override
+    public SutInfoDto.OutputFormat getPreferredOutputFormat() {
+        return SutInfoDto.OutputFormat.JAVA_JUNIT_5;
+    }
+
+    @Override
+    public List<AuthenticationDto> getInfoForAuthentication() {
+        return null;
+    }
+
+    public Connection getConnection() {
+        return connection;
+    }
+
+
+
+}
