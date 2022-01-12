1dcd70f368680acfda7bac41462ec4d7e197650c
Man Zhang <man.mzhang@gmail.com>
2022-01-12 13:07:38 +0100

    fix sut with clean db

--- dotnet_3/em/embedded/rest/SampleProjectDriver/EmbeddedEvoMasterController.cs
+++ dotnet_3/em/embedded/rest/SampleProjectDriver/EmbeddedEvoMasterController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Threading.Tasks;
 using Docker.DotNet;
 using EvoMaster.Controller;
@@ -10,24 +11,31 @@ using EvoMaster.DatabaseController;
 using EvoMaster.DatabaseController.Abstractions;
 using Microsoft.Data.SqlClient;
 
-namespace SampleProjectDriver
-{
-    public class EmbeddedEvoMasterController : EmbeddedSutController
-    {
+namespace SampleProjectDriver{
+    public class EmbeddedEvoMasterController : EmbeddedSutController{
         private bool _isSutRunning;
         private int _sutPort;
         private SqlConnection _connection;
         private IDatabaseController _databaseController;
+        private List<string> sqlCommands;
+        private List<string> clearDbCommands;
 
-        static void Main(string[] args)
-        {
-            var embeddedEvoMasterController = new EmbeddedEvoMasterController();
-
+        static void Main(string[] args){
+            int port = 40100;
+            if (args.Length > 0){
+                port = Int32.Parse(args[0]); 
+            }
+            
+            var embeddedEvoMasterController = new EmbeddedEvoMasterController(port);
             var instrumentedSutStarter = new InstrumentedSutStarter(embeddedEvoMasterController);
 
             Console.WriteLine("Driver is starting...\n");
 
             instrumentedSutStarter.Start();
+        }
+
+        public EmbeddedEvoMasterController(int port){
+            SetControllerPort(port);
         }
 
         public override string GetDatabaseDriverName() => null;
@@ -45,29 +53,29 @@ namespace SampleProjectDriver
 
         public override bool IsSutRunning() => _isSutRunning;
 
-        public override void ResetStateOfSut()
-        {
-            //TODO
-            DbCleaner.ClearDatabase(_connection, null, DatabaseType.MS_SQL_SERVER, "");
+        public override void ResetStateOfSut(){
+            DbCleaner.ClearDatabase(_connection, null, DatabaseType.MS_SQL_SERVER, "orders");
+            DbCleaner.ClearDatabase(_connection, null, DatabaseType.MS_SQL_SERVER, "app");
+            DbCleaner.ClearDatabase(_connection, null, DatabaseType.MS_SQL_SERVER, "payments");
+            
         }
 
-        public override string StartSut()
-        {
+        public override string StartSut(){
             var ephemeralPort = GetEphemeralTcpPort();
 
             const int timeout = 300;
 
-            Task.Run(async () =>
-            {
+            Task.Run(async () => {
                 var dbPort = GetEphemeralTcpPort();
 
-                _databaseController = new SqlServerDatabaseController("SampleCQRS", dbPort, "sqlpass@123", timeout, "mcr.microsoft.com/mssql/server:2017-CU14-ubuntu");
+                _databaseController = new SqlServerDatabaseController("SampleCQRS", dbPort, "sqlpass@123", timeout,
+                    "mcr.microsoft.com/mssql/server:2017-CU14-ubuntu");
 
                 var (connectionString, dbConnection) = await _databaseController.StartAsync();
 
                 _connection = dbConnection as SqlConnection;
 
-                SampleProject.API.Program.Main(new[] {ephemeralPort.ToString(), connectionString});
+                SampleProject.API.Program.Main(new[]{ephemeralPort.ToString(), connectionString});
             });
 
             WaitUntilSutIsRunning(ephemeralPort, timeout);
@@ -79,8 +87,7 @@ namespace SampleProjectDriver
             return $"http://localhost:{ephemeralPort}";
         }
 
-        public override void StopSut()
-        {
+        public override void StopSut(){
             SampleProject.API.Program.Shutdown();
             _databaseController.Stop();
             _isSutRunning = false;
