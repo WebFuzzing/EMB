1ab7fbb2ccc92bf01a0076bfd9d650994e27f3d1
asmab <asma.belhadi.17@gmail.com>
2022-01-19 13:11:46 +0100

    Updating the query resolver

--- jdk_8_maven/cs/graphql/scs-graphql/src/main/kotlin/org/graphqlncs/resolver/QueryResolver.kt
+++ jdk_8_maven/cs/graphql/scs-graphql/src/main/kotlin/org/graphqlncs/resolver/QueryResolver.kt
@@ -1,17 +1,41 @@
 package org.graphqlncs.resolver
 
 import graphql.kickstart.tools.GraphQLQueryResolver
-import org.graphqlncs.type.TriangleClassification
+import org.graphqlncs.type.*
 import org.springframework.stereotype.Component
 
 @Component
 open class QueryResolver(
-    private val triangleClassif: TriangleClassification
+    private val triangleClassification: TriangleClassification,
+    private val expint: Expint,
+    private val fisher: Fisher,
+    private val gammq: Gammq,
+    private val remainder: Remainder,
+    private val bessj: Bessj
 ) : GraphQLQueryResolver {
 
 
     fun triangleClassification(a: Int, b: Int, c: Int): Int {
-        return triangleClassif.classify(a, b, c)
+        return triangleClassification.classify(a, b, c)
     }
 
+    fun expint(n: Int, x: Double ): Double {
+        return expint.exe(n, x)
+    }
+
+    fun fisher(m: Int, n: Int, x:Double  ): Double {
+        return fisher.exe(m, n, x)
+    }
+
+    fun gammq(a: Double, x:Double  ): Double {
+        return gammq.exe(a, x)
+    }
+
+    fun remainder(a: Int, b:Int  ): Int {
+        return remainder.exe(a, b)
+    }
+
+    fun bessj(n:Int, x:Double  ): Double {
+        return bessj(n, x)
+    }
 }
