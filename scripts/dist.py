ab7bc98646038084a9dbf74377c3bcd1a03b4898
arcuri82 <arcuri82@gmail.com>
2022-03-24 14:27:58 +0100

    adding react-finland to build

--- scripts/dist.py
+++ scripts/dist.py
@@ -189,7 +189,7 @@ def build_js_npm():
     buildJS(os.path.abspath(os.path.join(PROJ_LOCATION, "js_npm","rest","disease-sh-api")), "disease-sh-api")
     buildJS(os.path.abspath(os.path.join(PROJ_LOCATION, "js_npm","rest","realworld-app")), "realworld-app")
     buildJS(os.path.abspath(os.path.join(PROJ_LOCATION, "js_npm","rest","spacex-api")), "spacex-api")
-
+    buildJS(os.path.abspath(os.path.join(PROJ_LOCATION, "js_npm","graphql","react-finland")), "react-finland")
 
 ####################
 def build_dotnet_3():
