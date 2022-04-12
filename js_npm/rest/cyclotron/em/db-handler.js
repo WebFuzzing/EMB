d5f8d07cff7ddaa5551428b9b2adee040733a0c4
arcuri82 <arcuri82@gmail.com>
2022-04-12 11:52:37 +0200

    fixed db handling

--- js_npm/rest/cyclotron/em/db-handler.js
+++ js_npm/rest/cyclotron/em/db-handler.js
@@ -1,20 +1,17 @@
 const { GenericContainer } = require("testcontainers");
 const mongoose = require('mongoose');
 
-let dbPort = 27017;
-let exposedDbPort = 0;
-let dbURL = "mongodb://localhost/cyclotron"
 let test_container = null;
 
 module.exports ={
     startDb: async () =>{
         console.log("start db")
-        dbPort = process.env.DB_PORT || 27017;
+        const dbPort = 27017;
         test_container= await new GenericContainer("mongo", "3.5")
             .withExposedPorts(dbPort)
             .start();
-        exposedDbPort = test_container.getMappedPort(dbPort)
-        dbURL = `mongodb://localhost:${exposedDbPort}/cyclotron`
+        const exposedDbPort = test_container.getMappedPort(dbPort)
+        const dbURL = `mongodb://localhost:${exposedDbPort}/cyclotron`
         process.env.DB_URL = dbURL;
 
         console.log("connecting "+dbURL);
@@ -26,9 +23,7 @@ module.exports ={
             setTimeout(checkdb, 300)
     },
 
-    getDbPort: () =>{
-        return exposedDbPort
-    },
+
     /*
         https://github.com/KristianWEB/fakebooker-backend/blob/471d6f6fafc95af57a99b6506c8f945dce43ffe9/jest.setup.js
         https://kb.objectrocket.com/postgresql/mongoose-drop-collection-if-exists-605
