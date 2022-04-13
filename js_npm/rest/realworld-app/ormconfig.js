a71579a715e41d56e2603db809e07c9d426b7d82
arcuri82 <arcuri82@gmail.com>
2022-04-13 14:33:24 +0200

    removed dependency to ENV var

--- js_npm/rest/realworld-app/ormconfig.js
+++ js_npm/rest/realworld-app/ormconfig.js
@@ -5,11 +5,11 @@ module.exports = {
     "username": "test",
     "password": "test",
     "database": "test",
-    "entities": [ process.env.NODE_ENV == 'production' ? __dirname + '/build/src/**/**.entity.js' : (process.env.NODE_ENV == 'em_production'? __dirname + '/instrumented/src/**/**.entity.js':__dirname + '/build/src/**/**.entity.js') ],
+    "entities": [ process.env.NODE_ENV == 'production' ? __dirname + '/build/src/**/**.entity.js' :  __dirname + '/instrumented/src/**/**.entity.js' ],
     "migrationsTableName": "migrations",
-    "migrations": [process.env.NODE_ENV == 'em_production'? "instrumented/src/database/migration/*.js": "build/src/database/migration/*.js"],
+    "migrations": [process.env.NODE_ENV == 'production'? "build/src/database/migration/*.js": "instrumented/src/database/migration/*.js"],
     "cli": {
-        "migrationsDir": process.env.NODE_ENV == 'em_production'? "instrumented/src/database/migration": "build/src/database/migration"
+        "migrationsDir": process.env.NODE_ENV == 'production'? "build/src/database/migration": "instrumented/src/database/migration"
     },
     "synchronize": true
 }
