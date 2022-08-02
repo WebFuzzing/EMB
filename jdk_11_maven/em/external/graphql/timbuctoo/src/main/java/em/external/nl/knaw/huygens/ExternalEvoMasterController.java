f87971ae865b31e512da7d04c565cd4105e90f47
arcuri82 <arcuri82@gmail.com>
2022-08-02 13:12:09 +0200

    increase memory allocation

--- jdk_11_maven/em/external/graphql/timbuctoo/src/main/java/em/external/nl/knaw/huygens/ExternalEvoMasterController.java
+++ jdk_11_maven/em/external/graphql/timbuctoo/src/main/java/em/external/nl/knaw/huygens/ExternalEvoMasterController.java
@@ -142,7 +142,7 @@ public class ExternalEvoMasterController extends ExternalSutController {
                 "-Ddw.databases.databaseLocation="+tmpFolder+"/datasets",
                 "-Ddw.databaseConfiguration.databasePath="+tmpFolder+"/neo4j",
                 "-Ddw.dataSet.dataStorage.rootDir=/datasets",
-                "-Xmx4G"
+                "-Xmx8G"
         };
     }
 
