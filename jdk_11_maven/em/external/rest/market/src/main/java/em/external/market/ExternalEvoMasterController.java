f3733f19be7782f953d80cc9dc91f6755c261175
arcuri82 <arcuri82@gmail.com>
2022-09-09 12:11:36 +0200

    Market external driver

---
+++ jdk_11_maven/em/external/rest/market/src/main/java/em/external/market/ExternalEvoMasterController.java
@@ -0,0 +1,222 @@
+package em.external.market;
+
+import org.evomaster.client.java.controller.AuthUtils;
+import org.evomaster.client.java.controller.ExternalSutController;
+import org.evomaster.client.java.controller.InstrumentedSutStarter;
+import org.evomaster.client.java.controller.api.dto.AuthenticationDto;
+import org.evomaster.client.java.controller.api.dto.SutInfoDto;
+import org.evomaster.client.java.controller.api.dto.database.schema.DatabaseType;
+import org.evomaster.client.java.controller.db.DbCleaner;
+import org.evomaster.client.java.controller.db.SqlScriptRunnerCached;
+import org.evomaster.client.java.controller.internal.db.DbSpecification;
+import org.evomaster.client.java.controller.problem.ProblemInfo;
+import org.evomaster.client.java.controller.problem.RestProblem;
+import org.h2.tools.Server;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+public class ExternalEvoMasterController extends ExternalSutController {
+
+    public static void main(String[] args) {
+
+        int controllerPort = 40100;
+        if (args.length > 0) {
+            controllerPort = Integer.parseInt(args[0]);
+        }
+        int sutPort = 12345;
+        if (args.length > 1) {
+            sutPort = Integer.parseInt(args[1]);
+        }
+        String jarLocation = "cs/rest-gui/market/market-rest/target";
+        if (args.length > 2) {
+            jarLocation = args[2];
+        }
+        if(! jarLocation.endsWith(".jar")) {
+            jarLocation += "/market-sut.jar";
+        }
+        int timeoutSeconds = 120;
+        if(args.length > 3){
+            timeoutSeconds = Integer.parseInt(args[3]);
+        }
+
+        String command = "java";
+        if(args.length > 4){
+            command = args[4];
+        }
+
+        ExternalEvoMasterController controller =
+                new ExternalEvoMasterController(controllerPort, jarLocation, sutPort, timeoutSeconds, command);
+        InstrumentedSutStarter starter = new InstrumentedSutStarter(controller);
+
+        starter.start();
+    }
+
+
+    private final int timeoutSeconds;
+    private final int sutPort;
+    private final int dbPort;
+    private  String jarLocation;
+    private Connection sqlConnection;
+    private List<DbSpecification> dbSpecification;
+    private Server h2;
+
+    public ExternalEvoMasterController() {
+        this(40100, "../core/target", 12345, 120, "java");
+    }
+
+    public ExternalEvoMasterController(String jarLocation) {
+        this();
+        this.jarLocation = jarLocation;
+    }
+
+    public ExternalEvoMasterController(int controllerPort, String jarLocation, int sutPort, int timeoutSeconds, String command) {
+        this.sutPort = sutPort;
+        this.dbPort = sutPort + 1;
+        this.jarLocation = jarLocation;
+        this.timeoutSeconds = timeoutSeconds;
+        setControllerPort(controllerPort);
+        setJavaCommand(command);
+    }
+
+    private String dbUrl( ) {
+
+        String url = "jdbc";
+        url += ":h2:tcp://localhost:" + dbPort + "/./temp/tmp_market/testdb_" + dbPort;
+
+        return url;
+    }
+
+    @Override
+    public String[] getInputParameters() {
+        return new String[]{
+                "--server.port=" + sutPort
+        };
+    }
+
+    public String[] getJVMParameters() {
+        return new String[]{
+                "-Dspring.datasource.url=" + dbUrl() + ";DB_CLOSE_DELAY=-1",
+                "-Dspring.datasource.username=sa",
+                "-Dspring.datasource.password"
+        };
+    }
+
+    @Override
+    public String getBaseURL() {
+        return "http://localhost:" + sutPort;
+    }
+
+    @Override
+    public String getPathToExecutableJar() {
+        return jarLocation;
+    }
+
+    @Override
+    public String getLogMessageOfInitializedServer() {
+        return "Started RestApplication in ";
+    }
+
+    @Override
+    public long getMaxAwaitForInitializationInSeconds() {
+        return timeoutSeconds;
+    }
+
+    @Override
+    public void preStart() {
+
+        try {
+            //starting H2
+            h2 = Server.createTcpServer("-tcp", "-tcpAllowOthers", "-tcpPort", "" + dbPort);
+            h2.start();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public void postStart() {
+        closeDataBaseConnection();
+
+        try {
+            Class.forName("org.h2.Driver");
+            sqlConnection = DriverManager.getConnection(dbUrl(), "sa", "");
+            dbSpecification = Arrays.asList(new DbSpecification(DatabaseType.H2,sqlConnection)
+                    .withDisabledSmartClean());
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+        SqlScriptRunnerCached.runScriptFromResourceFile(sqlConnection,"/schema.sql");
+    }
+
+    @Override
+    public void resetStateOfSUT() {
+        DbCleaner.clearDatabase_H2(sqlConnection);
+        SqlScriptRunnerCached.runScriptFromResourceFile(sqlConnection,"/data.sql");
+    }
+
+    @Override
+    public void preStop() {
+        closeDataBaseConnection();
+    }
+
+    @Override
+    public void postStop() {
+        if (h2 != null) {
+            h2.stop();
+        }
+    }
+
+    private void closeDataBaseConnection() {
+        if (sqlConnection != null) {
+            try {
+                sqlConnection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+            sqlConnection = null;
+        }
+    }
+
+    @Override
+    public String getPackagePrefixesToCover() {
+        return "market.";
+    }
+
+
+    @Override
+    public ProblemInfo getProblemInfo() {
+
+        return new RestProblem(
+                "http://localhost:" + sutPort + "/v2/api-docs",
+                null,
+                null
+        );
+    }
+
+    @Override
+    public SutInfoDto.OutputFormat getPreferredOutputFormat() {
+        return SutInfoDto.OutputFormat.JAVA_JUNIT_5;
+    }
+
+
+    @Override
+    public List<AuthenticationDto> getInfoForAuthentication() {
+        return Arrays.asList(
+                AuthUtils.getForBasic("admin","admin","password"),
+                AuthUtils.getForBasic("user", "ivan.petrov@yandex.ru", "petrov")
+        );
+    }
+
+
+
+    @Override
+    public List<DbSpecification> getDbSpecifications() {
+        return dbSpecification;
+    }
+}
