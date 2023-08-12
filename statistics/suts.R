c442d299bdadf5751f89e1c1b270de8a2b52bc5f
arcuri82 <arcuri82@gmail.com>
2023-08-12 10:03:53 +0200

    latex table for papers

---
+++ statistics/suts.R
@@ -0,0 +1,41 @@
+source("analyze.R")
+
+suts <- function(){
+
+  # This generated table should be added to Git, as will change based on papers
+  TABLE = "./table_suts.tex"
+
+  #Names here should match what in data.csv
+  SUTS = c(
+    "catwatch",
+    "cwa-verification",
+    "features-service",
+    "gestaohospital",
+    "languagetool",
+    "ocvn",
+    "proxyprint",
+    "rest-ncs",
+    "rest-news",
+    "rest-scs",
+    "restcountries",
+    "scout-api",
+    "genome-nexus",
+    "market",
+    #"petclinic-graphql",
+    #"patio-api",
+    #"timbuctoo",
+    #"graphql-ncs",
+    #"graphql-scs",
+    #"thrift-ncs",
+    #"thrift-scs",
+    #"grpc-ncs",
+    #"grpc-scs",
+    #"signal-registration",
+    "ind0",
+    #"ind1",
+    ""
+  )
+
+  latex(TABLE,SUTS)
+}
+
