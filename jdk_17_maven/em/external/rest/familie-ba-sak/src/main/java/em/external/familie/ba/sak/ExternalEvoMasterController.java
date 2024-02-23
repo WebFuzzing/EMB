3f54e7ea7a74fb59025d8fe61110d502c808d2d4
arcuri82 <arcuri82@gmail.com>
2024-02-23 15:53:13 +0100

    working on external

---
+++ jdk_17_maven/em/external/rest/familie-ba-sak/src/main/java/em/external/familie/ba/sak/ExternalEvoMasterController.java
@@ -0,0 +1,229 @@
+package em.external.familie.ba.sak;
+
+import org.evomaster.client.java.controller.ExternalSutController;
+import org.evomaster.client.java.controller.InstrumentedSutStarter;
+import org.evomaster.client.java.controller.api.dto.auth.AuthenticationDto;
+import org.evomaster.client.java.controller.api.dto.SutInfoDto;
+import org.evomaster.client.java.controller.api.dto.database.schema.DatabaseType;
+import org.evomaster.client.java.sql.DbCleaner;
+import org.evomaster.client.java.sql.SqlScriptRunner;
+import org.evomaster.client.java.sql.SqlScriptRunnerCached;
+import org.evomaster.client.java.sql.DbSpecification;
+import org.evomaster.client.java.controller.problem.GraphQlProblem;
+import org.evomaster.client.java.controller.problem.ProblemInfo;
+import org.testcontainers.containers.GenericContainer;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class ExternalEvoMasterController extends ExternalSutController {
+
+
+    public static void main(String[] args) {
+
+        int controllerPort = 40100;
+        if (args.length > 0) {
+            controllerPort = Integer.parseInt(args[0]);
+        }
+        int sutPort = 12345;
+        if (args.length > 1) {
+            sutPort = Integer.parseInt(args[1]);
+        }
+        String jarLocation = "cs/rest/familie-ba-sak/target";
+        if (args.length > 2) {
+            jarLocation = args[2];
+        }
+        if(! jarLocation.endsWith(".jar")) {
+            jarLocation += "/familie-ba-sak-sut.jar";
+        }
+
+        int timeoutSeconds = 120;
+        if(args.length > 3){
+            timeoutSeconds = Integer.parseInt(args[3]);
+        }
+        String command = "java";
+        if(args.length > 4){
+            command = args[4];
+        }
+
+
+        ExternalEvoMasterController controller =
+                new ExternalEvoMasterController(controllerPort, jarLocation,
+                        sutPort, timeoutSeconds, command);
+        InstrumentedSutStarter starter = new InstrumentedSutStarter(controller);
+
+        starter.start();
+    }
+
+    private final int timeoutSeconds;
+    private final int sutPort;
+    private  String jarLocation;
+    private Connection sqlConnection;
+    //private String INIT_DB_SCRIPT_PATH = "/populateDB.sql";
+
+    private List<DbSpecification> dbSpecification;
+
+    private String initSQLScript;
+
+    private static final String POSTGRES_VERSION = "13.13";
+
+    private static final String POSTGRES_PASSWORD = "password";
+
+    private static final int POSTGRES_PORT = 5432;
+
+    private static final GenericContainer postgres = new GenericContainer("postgres:" + POSTGRES_VERSION)
+            .withEnv("POSTGRES_PASSWORD", POSTGRES_PASSWORD)
+            .withEnv("POSTGRES_HOST_AUTH_METHOD", "trust") //to allow all connections without a password
+            .withEnv("POSTGRES_DB", "familiebasak")
+            .withTmpFs(Collections.singletonMap("/var/lib/postgresql/data", "rw"))
+            .withExposedPorts(POSTGRES_PORT);
+
+    public ExternalEvoMasterController(){
+        this(40100, "../core/target", 12345, 120, "java");
+    }
+
+    public ExternalEvoMasterController(String jarLocation) {
+        this();
+        this.jarLocation = jarLocation;
+    }
+
+    public ExternalEvoMasterController(
+            int controllerPort, String jarLocation, int sutPort, int timeoutSeconds, String command
+           ) {
+
+        if(jarLocation==null || jarLocation.isEmpty()){
+            throw new IllegalArgumentException("Missing jar location");
+        }
+
+
+        this.sutPort = sutPort;
+        this.jarLocation = jarLocation;
+        this.timeoutSeconds = timeoutSeconds;
+        setControllerPort(controllerPort);
+        setJavaCommand(command);
+    }
+
+
+    @Override
+    public String[] getInputParameters() {
+        return new String[]{"--server.port=" + sutPort};
+    }
+
+    public String[] getJVMParameters() {
+
+        return new String[]{
+                "-Dspring.datasource.url=" + dbUrl(),
+                "-Dspring.cache.type=none",
+                "-Dspring.profiles.active=postgresql,spring-data-jpa",
+                "-Dspring.jmx.enabled=false",
+        };
+    }
+
+    private String dbUrl() {
+
+        String host = postgres.getContainerIpAddress();
+        int port = postgres.getMappedPort(5432);
+
+        String url = "jdbc";
+        url += ":postgresql://"+host+":"+port+"/petclinic";
+
+        return url;
+    }
+
+    @Override
+    public String getBaseURL() {
+        return "http://localhost:" + sutPort;
+    }
+
+    @Override
+    public String getPathToExecutableJar() {
+        return jarLocation;
+    }
+
+    @Override
+    public String getLogMessageOfInitializedServer() {
+        return "Tomcat started on port";
+    }
+
+    @Override
+    public long getMaxAwaitForInitializationInSeconds() {
+        return timeoutSeconds;
+    }
+
+    @Override
+    public void preStart() {
+        postgres.start();
+    }
+
+    @Override
+    public void postStart() {
+        closeDataBaseConnection();
+
+        try {
+            sqlConnection = DriverManager.getConnection(dbUrl(), "postgres", POSTGRES_PASSWORD);
+
+            dbSpecification = Arrays.asList(new DbSpecification(DatabaseType.POSTGRES,sqlConnection)
+                    .withSchemas("public")
+            );
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public void resetStateOfSUT() {
+    }
+
+    @Override
+    public void preStop() {
+        closeDataBaseConnection();
+    }
+
+    @Override
+    public void postStop() {
+        postgres.stop();
+    }
+
+    private void closeDataBaseConnection() {
+        if (sqlConnection != null) {
+            try {
+                sqlConnection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+            sqlConnection = null;
+        }
+    }
+
+    @Override
+    public String getPackagePrefixesToCover() {
+        return "org.springframework.samples.petclinic";
+    }
+
+    @Override
+    public ProblemInfo getProblemInfo() {
+        return new GraphQlProblem("/graphql");
+    }
+
+    @Override
+    public SutInfoDto.OutputFormat getPreferredOutputFormat() {
+        return SutInfoDto.OutputFormat.JAVA_JUNIT_5;
+    }
+
+
+    @Override
+    public List<AuthenticationDto> getInfoForAuthentication() {
+        return null;
+    }
+
+    @Override
+    public List<DbSpecification> getDbSpecifications() {
+        return dbSpecification;
+    }
+}
