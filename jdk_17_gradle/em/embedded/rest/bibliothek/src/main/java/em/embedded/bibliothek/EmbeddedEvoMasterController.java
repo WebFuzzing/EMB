a8323101035cf850331cd16c682b821dcbf8f799
Onur D <dumanonur@gmail.com>
2024-10-11 13:13:58 +0200

    Revert "First attempt to add JWT tokens to bibliothek."
    This reverts commit ace1a0aec02c8e97cffdd8163b40ad3bd7dbbf5a.

--- jdk_17_gradle/em/embedded/rest/bibliothek/src/main/java/em/embedded/bibliothek/EmbeddedEvoMasterController.java
+++ jdk_17_gradle/em/embedded/rest/bibliothek/src/main/java/em/embedded/bibliothek/EmbeddedEvoMasterController.java
@@ -3,7 +3,6 @@ package em.embedded.bibliothek;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 import io.papermc.bibliothek.BibliothekApplication;
-import org.evomaster.client.java.controller.AuthUtils;
 import org.evomaster.client.java.controller.EmbeddedSutController;
 import org.evomaster.client.java.controller.InstrumentedSutStarter;
 import org.evomaster.client.java.controller.api.dto.auth.AuthenticationDto;
@@ -14,11 +13,8 @@ import org.evomaster.client.java.controller.problem.RestProblem;
 import org.springframework.boot.SpringApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.testcontainers.containers.GenericContainer;
-import org.testcontainers.containers.startupcheck.OneShotStartupCheckStrategy;
 
 
-import java.time.Duration;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import java.util.Collections;
@@ -56,7 +52,6 @@ public class EmbeddedEvoMasterController extends EmbeddedSutController {
     private static final GenericContainer mongodbContainer = new GenericContainer("mongo:" + MONGODB_VERSION)
             .withTmpFs(Collections.singletonMap("/data/db", "rw"))
             .withExposedPorts(MONGODB_PORT);
-    //        .withStartupCheckStrategy(new OneShotStartupCheckStrategy().withTimeout(Duration.ofSeconds(90)));
 
     private String mongoDbUrl;
 
@@ -75,11 +70,9 @@ public class EmbeddedEvoMasterController extends EmbeddedSutController {
     public String startSut() {
 
         mongodbContainer.start();
-
         mongoDbUrl = "mongodb://" + mongodbContainer.getContainerIpAddress() + ":" + mongodbContainer.getMappedPort(MONGODB_PORT) + "/" + MONGODB_DATABASE_NAME;
         mongoClient = MongoClients.create(mongoDbUrl);
 
-        //mongodbContainer.setStartupCheckStrategy( new OneShotStartupCheckStrategy() );
 
 
         ctx = SpringApplication.run(BibliothekApplication.class,
@@ -132,28 +125,7 @@ public class EmbeddedEvoMasterController extends EmbeddedSutController {
     @Override
     public List<AuthenticationDto> getInfoForAuthentication() {
         //TODO might need to setup JWT headers here
-        System.out.println("HERE");
-
-        // @Onur Setting JWT Authentication here.
-        return Arrays.asList(
-                AuthUtils.getForJWT(
-                        "User1",
-                        "/api/auth_token/auth1",
-                        """
-                                {"userId": "User1", "password":"User1_Password123"}
-                            """.trim().stripIndent(),
-                        "/token/authToken1"),
-                AuthUtils.getForJWT(
-                        "User2",
-                        "/api/auth_token/auth2",
-                        """
-                                {"userId": "User2", "password":"User2_Password456"}
-                            """.trim().stripIndent(),
-                        "/token/authToken2")
-                );
-
-
-        //return null;
+        return null;
     }
 
 
